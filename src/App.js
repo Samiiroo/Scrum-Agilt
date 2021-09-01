5623fb4267e35719f554b79292e017751185a4eb
Tommy Danielsson <tdserviceco@gmail.com>
2021-09-01 14:23:37 +0200

    will pause here and complete about us first

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import './scss/_reset.scss';
 import Articles from './components/Articles';
 function App(props) {
-
   useEffect(() => {
     document.title = 'Agile Tamam'
   }, [])
