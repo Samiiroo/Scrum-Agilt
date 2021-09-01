0f6a3fb07c1ce20304520c4d998189b6471de83a
Muhannad Abbasi <58631937+Muhannad-Abbasi@users.noreply.github.com>
2021-09-01 12:50:18 +0200

    .

--- src/App.js
+++ src/App.js
@@ -2,42 +2,12 @@ import React, { useEffect } from 'react';
 import { useState } from "react";
 import './scss/_reset.scss';
 import Faq from './components/Faq';
+import data from './components/data.json'
 import './scss/components/faq.css';
 
 function App(props) {
 
-  const [ faqs, setFaqs ] = useState([
-    {
-      question: 'How are you',
-      answer: 'I am good',
-      open: false
-    },
-    {
-      question: 'Whast is your name',
-      answer: 'my name is jeff',
-      open: false
-    },
-    {
-      question: 'How old are you',
-      answer: 'I am 25y/o',
-      open: false
-    },
-    {
-      question: 'where are you from',
-      answer: 'I am from korea',
-      open: false
-    },
-    {
-      question: 'what is up',
-      answer: 'noiceee',
-      open: false
-    },
-    {
-      question: 'hmmmm',
-      answer: 'hmmmmmmmmmmmmm',
-      open: false
-    },
-  ])
+  const [ faqs, setFaqs ] = useState(data)
 
   const toggleFaq = (index) => {
     setFaqs(faqs.map( (faq, i) => {
