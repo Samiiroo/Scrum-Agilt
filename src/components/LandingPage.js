a0023a1a7ad2c7594ff42f07fe844272446a0e88
Tommy Danielsson <tdserviceco@gmail.com>
2021-09-03 12:14:07 +0200

    fixed the future for sprint 2

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -2,14 +2,16 @@ import React from 'react';
 import CTA from './CTA';
 function LandingPage(props) {
   return (
-    <section className="landing-page" style={{ background: `url('/assets/img/landing-page-bg.png) no-repeat center/cover` }}>
-      <p>
-        Alltid undrat hur man kan behålla sin lugna tillstånd medan man bygger en perfekt sida som är
-        både smidig och fin?
-      </p>
-      <p>
-        Vi på Agile Taman lovar att vi är de mest flexibla arbetarna ni kommer hitta inom Skåne.
-      </p>
+    <section className="landing-page" style={{ background: `url('/assets/img/landing-page-bg.png') no-repeat center/cover` }}>
+      <div class="paragraphs">
+        <p class="center">
+          Alltid undrat hur man kan behålla sin lugna tillstånd medan man bygger en perfekt sida som är
+          både smidig och fin?
+        </p>
+        <p class="center">
+          Vi på Agile Taman lovar att vi är de mest flexibla arbetarna ni kommer hitta inom Skåne.
+        </p>
+      </div>
       <CTA />
     </section >
   );
