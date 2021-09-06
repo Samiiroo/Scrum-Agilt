d3ec0a2019cce42444f357a0e49df6d43b36f907
Tara78 <70331037+Tara78@users.noreply.github.com>
2021-09-06 14:34:04 +0200

    button
    Button connected to contact page

--- src/components/CTA.js
+++ src/components/CTA.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import '../scss/components/_cta.scss';
+import {Anchor} from 'antd';
+const {Link} = Anchor; 
+
 function CTA(props) {
   return (
     <div className="cta">
       <h2 className="title center">Vi designar din Hemsida från
         <span>400 kr/tim</span>
       </h2>
-      <a href="#contact" type="button" className="btn btn-cta center">Kontakta oss</a>
+      {/* <a href="#contact" type="button" className="btn btn-cta center">Kontakta oss</a> */}
+      <Anchor className="btn btn-cta center" targetOffset='80'><Link href="#about" title="Kontakta oss" /></Anchor>
+    
+      
     </div>
   );
 }
