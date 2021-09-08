6b0455d6e4731eb7f6ee69d3bedc51837b6f3f3d
Tara78 <70331037+Tara78@users.noreply.github.com>
2021-09-08 13:21:29 +0200

    klart
    klart

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState}from 'react';
 import '../scss/components/_footer.scss';
 
 import { Anchor } from 'antd';
@@ -6,36 +6,52 @@ import { Anchor } from 'antd';
 const { Link } = Anchor;
 
 function Footer() {
-  
+  const [showScroll, setShowScroll] = useState(false);
+
+
+
+  const checkScrollTop = () => {
+
+    if (!showScroll && window.pageYOffset > 400) {
+
+      setShowScroll(true);
+
+    } else if (showScroll && window.pageYOffset <= 400) {
+      setShowScroll(false);
+    }
+  };
+
+  const scrollTop = () => {
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  window.addEventListener("scroll", checkScrollTop);
   return (
     <footer>
-     {/* <Anchor>
-        <Link href="#landingpage" >
       <div className="arrow-up">
-      
-        <i className="fas fa-angle-double-up fa-2x"></i>
-    
+        <i className="fas fa-angle-double-up fa-2x" onClick={scrollTop}></i>
       </div>
-        </Link>
-      </Anchor> */}
-      <div className="arrow-up">
-        <i className="fas fa-angle-double-up fa-2x"></i>
-       
+      <Anchor targetOffset="80">
+        <ul className="footer-list">
+          <li>
+            <Link href="#about" title="Om oss" />
+          </li>
+          <li>
+            <Link href="#faq" title="Vanliga frågor" />
+          </li>
 
-      </div>
-      <ul className="footer-list">
-  
-        <li>
-          <a href="#about">Om oss</a>
-        </li>
-        <li>
-          <a href="#faq">Vanliga frågor</a>
-        </li>
-        <li>
-          <a href="#contact">Kontakt</a>
-        </li>
-      </ul>
+          <li>
+
+            <Link href="#contact" title="Kontakt" />
+
+          </li>
+
+        </ul>
+
+      </Anchor>
+
       <div className="copyright">Copyright: Inc.2021 all reserved</div>
+     
     </footer>
   )
 }
